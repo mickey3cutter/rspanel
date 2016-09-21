6801d8bd1ee4eba0b210cfb76593a6c848a2aa3b
mickey3cutter <mickey3cutter@gmail.com>
2016-09-21 14:20:17 +0300

    first upload

---
+++ admin-theme.php
@@ -0,0 +1,221 @@
+<?php
+
+/*
+Plugin Name: RS Panel
+Author: Redstone
+Author uri: https://redstone.media
+Version: 1.0
+*/
+
+
+
+
+//Update
+
+add_action( 'init', 'github_plugin_updater_test_init' );
+function github_plugin_updater_test_init() {
+
+	include_once 'updater.php';
+
+	define( 'WP_GITHUB_FORCE_UPDATE', true );
+
+	if ( is_admin() ) { // note the use of is_admin() to double check that this is happening in the admin
+
+		$config = array(
+			'slug' => plugin_basename( __FILE__ ),
+			'proper_folder_name' => 'gilpanel',
+			'api_url' => 'https://api.github.com/repos/mickey3cutter/rspanel',
+			'raw_url' => 'https://raw.github.com/mickey3cutter/rspanel/master',
+			'github_url' => 'https://github.com/mickey3cutter/rspanel',
+			'zip_url' => 'https://github.com/mickey3cutter/rspanel/archive/master.zip',
+			'sslverify' => true,
+			'requires' => '4.0',
+			'tested' => '4.5.2',
+			'readme' => 'README.md',
+			'access_token' => '',
+		);
+
+		new WP_GitHub_Updater( $config );
+
+	}
+
+}
+
+
+
+//Initialize plugin
+function m3c_settings_init() {
+    register_setting( 'm3c_plugin_settings', 'm3c_option' );
+}
+function register_m3c_menu_page() {
+	add_menu_page( 'Options', 'RS Panel', 'manage_options', 'rspanel', 'm3c_admin_page', 'dashicons-album' );
+}
+add_action( 'admin_init', 'm3c_settings_init' );
+add_action( 'admin_menu', 'register_m3c_menu_page' );
+function m3c_admin_page(){
+	require 'admin_page.php';
+}
+
+//Globalize options variable
+$option = get_option( 'm3c_option' ); 
+
+//Redirect in admin menu
+$req_uri = substr($_SERVER['REQUEST_URI'],-17);
+if($req_uri == '/wp-admin/rspanel' || $req_uri == 'wp-admin/rspanel/'){
+	header("Location: admin.php?page=rspanel");
+	die();
+}
+
+
+//Remove menus
+function m3c_remove_menus(){
+	global $option;
+	$pages_old = $GLOBALS[ 'admin_page_hooks' ];
+	$pages_new = get_option('m3c_menu_positions', true);
+	$pages = explode(',', $pages_new);
+	if($pages[0]=='dashboard' || $pages_new=='1'){
+		$pages = array_keys($pages_old);
+	}
+	foreach ($pages as $page) {
+		if(isset($option[$page])==1) { remove_menu_page( $page ); }
+	}
+
+}
+add_action( 'admin_menu', 'm3c_remove_menus',999 );
+
+//Add gilpanel view
+if(isset($option['gil'])){
+	$color = $option['color'];
+	$oldHash = get_transient('m3c_color');
+	$currentHash =  $option['color'];
+	if($oldHash !== $currentHash){
+	  set_transient('m3c_color', $currentHash, 60 * 60 * 24 * 14);
+	}
+	function gil_custom_fonts() {
+	  include ('rs_view.php');
+	}
+	add_action('admin_footer', 'gil_custom_fonts');
+	function gil_login_logo() {
+	   include ('rs_view.php');
+	}
+	add_action( 'login_enqueue_scripts', 'gil_login_logo' );
+	
+
+	//Change footer text
+
+	function gil_remove_footer_admin () 
+	{
+	    echo '<span id="footer-thankyou">Styled by <a href="http://redstone.media" target="_blank" rel="nofollow">REDSTONE</a></span>';
+	}
+	add_filter('admin_footer_text', 'gil_remove_footer_admin');
+
+	add_action( 'admin_enqueue_scripts', 'register_plugin_styles' );
+	function register_plugin_styles() {
+		wp_register_style( 'default_rs_style', plugin_dir_url( __FILE__ ) . 'css/default_rs_style.css'  );
+		wp_enqueue_style( 'default_rs_style' );
+	}
+
+	
+}
+
+//Add script to admin panel
+if(isset($option['js'])){
+	function m3c_enqueue() {    
+	    wp_enqueue_script( 'm3c_custom_script', plugin_dir_url( __FILE__ ) . 'js/admin.js' );
+	}
+	add_action('admin_enqueue_scripts', 'm3c_enqueue');
+}
+
+//Add Dasboard and Login CSS
+if(isset($option['css'])){
+	function m3c_custom_fonts() {
+	  echo '<link rel="stylesheet" href="'. plugin_dir_url( __FILE__ ) .'css/admin_style.css" type="text/css" media="all" />';
+	}
+	add_action('admin_head', 'm3c_custom_fonts');
+	function m3c_login_logo() {
+	    echo '<link rel="stylesheet" href="'. plugin_dir_url( __FILE__ ) .'css/admin_style.css" type="text/css" media="all" />';
+	 }
+	add_action( 'login_enqueue_scripts', 'm3c_login_logo' );
+}
+
+
+//Change login logo url
+if(isset($option['logo_url'])){
+	function m3c_login_logo_url() {
+	    return home_url();
+	}
+	add_filter( 'login_headerurl', 'm3c_login_logo_url' );
+}
+
+//Change footer text
+if(isset($option['footer_admin'])){
+	function m3c_remove_footer_admin () {
+		global $option;
+	    echo '<span id="footer-thankyou">'.$option['footer_text'].'</span>';
+	}
+	add_filter('admin_footer_text', 'm3c_remove_footer_admin');
+}
+function m3c_admin_head_func(){
+	echo '<style>
+	#advanced-custom-fields-pro,#toplevel_page_edit-post_type-acf-field-group{display:none}
+	</style>';
+}
+add_action('admin_head','m3c_admin_head_func');
+
+//Remove admin bar on front-end
+if(isset($option['admin_bar'])){
+	add_filter('show_admin_bar', '__return_false', 999);
+}
+
+
+
+
+
+
+add_action('wp_ajax_update_menu_positions', 'm3c_update_menu_positions');
+add_action('admin_enqueue_scripts', 'm3c_admin_enqueues');
+
+/* Filters */
+
+add_filter('custom_menu_order', 'm3c_custom_menu_order');
+add_filter('menu_order', 'm3c_custom_menu_order');
+
+/* Functions */
+
+function m3c_update_menu_positions() {
+    update_option('m3c_menu_positions', str_replace('admin.php?page=', '', $_REQUEST['menu_item_positions'])); // str_replace (support for custom added menu items)
+
+}
+
+function m3c_admin_enqueues() {
+	
+	    wp_enqueue_script('jquery-ui-sortable');
+	    wp_enqueue_script('amr_admin', plugins_url('/js/amr-admin.js', __FILE__), array('jquery-ui-sortable'));
+	    wp_enqueue_script('jsdelivr', 'https://cdn.jsdelivr.net/ace/1.2.3/min/ace.js', array('jquery-ui-sortable'));
+	    wp_enqueue_script('jquery');
+	    wp_enqueue_script('thickbox');
+	    wp_enqueue_style('thickbox');
+	    wp_enqueue_script('media-upload');
+   
+}
+
+function m3c_custom_menu_order($menu_order) {
+    if (!$menu_order)
+        return true;
+
+    $new_menu_order = get_option('m3c_menu_positions', true);
+
+    if ($new_menu_order) {
+        $new_menu_order = explode(',', $new_menu_order);
+
+        return $new_menu_order;
+    } else {
+        return $menu_order;
+    }
+}
+
+//Change login logo url
+add_filter( 'login_headerurl', 'custom_loginlogo_url' );
+function custom_loginlogo_url($url) {
+	return 'http://redstone.media/';
+}
---
+++ admin_page.php
@@ -0,0 +1,85 @@
+<?php m3c_admin_enqueues(); ?>
+<script src="http://malsup.github.com/jquery.form.js"></script>
+<link rel='stylesheet' id='imgareaselect-css'  href='<?php echo plugin_dir_url( __FILE__ ) ?>css/plugin_page.css' type='text/css' media='all' />
+<script type='text/javascript' src="<?php echo plugin_dir_url( __FILE__ ) ?>js/plugin_page.js"></script>
+
+<div class="content">
+	<form method="post" action="options.php" id="myOptionsForm">
+	<legend class="act">Hide & Sort admin menu </legend>
+	<fieldset class="act">
+		<div class="form-group" id="gil_list">
+			<?php 
+			
+			settings_fields( 'm3c_plugin_settings' );
+			$option = get_option( 'm3c_option' ); 
+			$pages_old = $GLOBALS[ 'admin_page_hooks' ];
+			$pages_new = get_option('m3c_menu_positions', true);
+			$pages = explode(',', $pages_new);
+			if($pages[0]=='dashboard' || $pages_new=='1'){
+				$pages = array_keys($pages_old);
+			}
+			//$count = -1;
+			//$pages_uri = array_keys($pages11);
+
+			foreach ($pages as $page) {  ?>
+				<label class="m3c_<?php echo strtolower($page); ?>"><input id="m3c_option[<?php echo $page; ?>]" data-href="<?php echo $page; ?>" name="m3c_option[<?php echo $page; ?>]" type="checkbox" value="1" <?php checked( '1', isset($option[$page] )); ?> /> 
+					<a href="<?php if(substr($page,0,3)!='edi' && substr($page,-3)!='php') { echo 'admin.php?page='.$page; } else { echo $page; } ?>"><?php echo ucfirst($pages_old[$page]); ?> </a></label>
+			<?php }?>
+		</div>
+	</fieldset>
+	<legend>Add custom view</legend>
+	<fieldset>
+		<div class="form-group">
+			<label><input id="m3c_option[css]" name="m3c_option[css]" type="checkbox" value="1" <?php checked( '1', isset($option['css'] )); ?> /> <?php echo 'Add custom CSS' ?> </label>
+			<label><input id="m3c_option[js]" name="m3c_option[js]" type="checkbox" value="1" <?php checked( '1', isset($option['js'] )); ?> /> <?php echo 'Add custom JS' ?> </label>
+			<label><input id="m3c_option[logo_url]" name="m3c_option[logo_url]" type="checkbox" value="1" <?php checked( '1', isset($option['logo_url'] )); ?> /> <?php echo 'Change login logo url to site homepage' ?> </label>
+			<label class="check_open"><input id="m3c_option[gil]" name="m3c_option[gil]" type="checkbox" value="1" <?php checked( '1', isset($option['gil'] )); ?> /> <?php echo 'Add Redstone view' ?> </label>
+			<div class="input_open">
+				<label><input id="m3c_option[color]"  name="m3c_option[color]" type="text" value='<?php echo $option['color']; ?>' placeholder="Change main color"/></label>
+				<label><input type="text" id="m3c_option[logo]" name="m3c_option[logo]" value="<?php echo  $option['logo']; ?>" data-id="upload_image" placeholder="Change logo"/>
+	        	<input id="upload_logo_button" type="button" class="button" value="<?php _e( 'Upload Logo', 'rs_panel' ); ?>" /></label>
+	        	<label>Custom CSS<textarea id="m3c_option[admin_css]"  name="m3c_option[admin_css]" type="text" value='<?php echo $option['admin_css']; ?>' placeholder="Main color" data-id="admin-css"/><?php echo $option['admin_css']; ?></textarea></label>
+		        <div id="admin_css"></div>
+	    	</div>
+		</div>
+	</fieldset>
+	<legend>Other</legend>
+	<fieldset>
+		<div class="form-group">
+			<label><input id="m3c_option[admin_bar]" name="m3c_option[admin_bar]" type="checkbox" value="1" <?php checked( '1', isset($option['admin_bar'] )); ?> /> <?php echo 'Remove admin bar' ?> </label>
+		</div>
+	</fieldset>
+	<p><input name="submit" id="submit" value="Save" type="submit"></p>
+	</form>
+	<div id="saveResult"></div>
+</div>
+
+<script>
+		var editor = ace.edit("admin_css");
+		editor.setTheme("ace/theme/monokai");
+		editor.getSession().setMode("ace/mode/css");
+		var textarea = jQuery('textarea[data-id="admin-css"]');
+		editor.getSession().setValue(textarea.val());
+		editor.getSession().on('change', function(){
+		  textarea.val(editor.getSession().getValue());
+		});
+	</script>
+
+<?php if( isset($option['admin_css'])) { echo '<style>'.$option["admin_css"].'</style>'; } ?>
+<!-- Add ajax save btn -->
+<!--  <script type="text/javascript">
+jQuery(document).ready(function($) {
+	 $('#myOptionsForm').submit(function() { 
+
+      $(this).ajaxSubmit({
+         success: function(){
+            $('#saveResult').html("<div id='saveMessage' class='successModal'></div>");
+            $('#saveMessage').append("<p><?php echo htmlentities(__('Settings Saved Successfully','wp'),ENT_QUOTES); ?></p>").show();
+         }, 
+         timeout: 5000
+      }); 
+      setTimeout("$('#saveMessage').hide('slow');", 5000);
+      return false; 
+   });
+});
+</script> -->
